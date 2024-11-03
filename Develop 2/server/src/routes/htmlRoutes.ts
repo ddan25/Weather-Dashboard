747874cecb86986d6135b76380fa064794416b14
ddan25 <peterortiz97@yahoo.com>
2024-11-03 14:05:03 -0800

    some fixes

--- Develop 2/server/src/routes/htmlRoutes.ts
+++ Develop 2/server/src/routes/htmlRoutes.ts
@@ -6,11 +6,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
-// Define route to serve index.html
-router.get('/', (req, res) => {
-    const requestMethod = req.method; // Example usage
-    console.log(`Request method: ${requestMethod}`); // Log the method (optional)
-    res.sendFile(path.join(__dirname, 'index.html')); // Adjust this path
+router.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
 });
 
 export default router;
